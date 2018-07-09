8b1e050f8b216045e1248c9c3e7d431fb1a077d9
Cam Prier <CSPrier@gmail.com>
2018-07-09 13:59:33 -0700

    Started heroku deployment

--- index.js
+++ index.js
@@ -16,14 +16,17 @@ const { dbConnect } = require('./db-mongoose');
 const userRouter = require('./users/routes/user');
 const authRouter = require('./users/routes/auth');
 
+// Express app
 const app = express();
 
+// MORGAN
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
   })
 );
 
+// CORS
 app.use(
   cors({
     origin: CLIENT_ORIGIN
@@ -33,12 +36,15 @@ app.use(
 // Parse requiest body
 app.use(express.json());
 
+// Auth
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
+// Endpoints
 app.use('/api', authRouter);
 app.use('/api/users', userRouter);
 
+// Catch-all 404
 app.use(function(req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
