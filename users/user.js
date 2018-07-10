b12b0331317081cf8d43edd5551c6569eb78f93f
Cam Prier <CSPrier@gmail.com>
2018-07-10 14:40:00 -0700

    Testing subdocs in userschema

--- users/user.js
+++ users/user.js
@@ -2,11 +2,12 @@
 
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+const questionSchema = require('../questions/questions');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, default: '', unique: true },
   password: { type: String, require: true },
-  questions: { type: Object }
+  questions: [questionSchema]
 }, { timestamps: true });
 
 userSchema.set('toObject', {
