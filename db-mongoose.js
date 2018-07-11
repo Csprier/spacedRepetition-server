831a4ebfd3af23895b82bde876e2b3aea511dd3b
Cam Prier <CSPrier@gmail.com>
2018-07-11 11:53:30 -0700

    Added  { useNewUrlParser: true } to dbConnect function in db-mongoose.js

--- db-mongoose.js
+++ db-mongoose.js
@@ -7,7 +7,7 @@ const { DATABASE_URL } = require('./config');
 
 function dbConnect(url = DATABASE_URL) {
   // console.log(DATABASE_URL);
-  return mongoose.connect(url)
+  return mongoose.connect(url, { useNewUrlParser: true })
     .catch(err => {
       console.error('Mongoose failed to connect');
       console.error(err);
